c61eccf9b7605e4fa4c964f7d646d5c4ccb0bd29
plongdev <phamlongsoftware@gmail.com>
2025-08-27 19:30:45 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,380 @@
+// APP DATA CONFIGURATION
+
+const apps = [
+    {
+        name: '𝐋𝐢𝐞̂𝐧 𝐐𝐮𝐚̂𝐧 𝐌𝐨𝐛𝐢𝐥𝐞 Hack Map ✅ ',
+        url: 'https://sub4unlock.io/6Wujc',
+        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/62/cb/24/62cb2406-91ce-f31c-9b6b-6397b88afe24/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/512x512bb.jpg',
+        descriptions: 'Hack Map Đánh Kín Update antiband fix 1 game band Cập Nhật 16/8/2025'
+    },
+    {
+        name: '𝐋𝐢𝐞̂𝐧 𝐐𝐮𝐚̂𝐧 𝐌𝐨𝐛𝐢𝐥𝐞 Mod Skin ✅',
+        url: 'https://sub4unlock.io/78mpX',
+        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/62/cb/24/62cb2406-91ce-f31c-9b6b-6397b88afe24/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/512x512bb.jpg',
+        descriptions: 'Bản Mod Skin Update antiband fix 1 game band cập nhật 16/8/2025'
+    },
+    {
+        name: 'TrollStore X ✅',
+        url: 'https://sub4unlock.io/rCHxp',
+        img: 'https://raw.githubusercontent.com/alfiecg24/TrollInstallerX/main/Resources/Icon.png',
+        descriptions: 'Hỗ Trợ iOS 14.0-16.6.1 Cập Nhật 15/8/2025'
+    },
+    {
+        name: 'Krnl VN ✅',
+        url: 'https://sub4unlock.io/CvXZN',
+        img: 'https://loadly.io/image/view/app_icons/7a9fc85a1a0219b1bd1d1bc2af25b2de/120',
+        descriptions: 'Mod Roblox Cre Huymythic Cập Nhật 15/8/2025'
+    },
+    {
+        name: 'DeltaVN ✅',
+        url: 'https://sub4unlock.io/ftV5e',
+        img: 'https://loadly.io/image/view/app_icons/7a9fc85a1a0219b1bd1d1bc2af25b2de/120',
+        descriptions: 'Mod Roblox Cre Huymythic Cập Nhật 15/8/2025'
+    },{
+        name: 'File iPA Liên Quân',
+        url: 'https://sub4unlock.io/vRUnk',
+        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/62/cb/24/62cb2406-91ce-f31c-9b6b-6397b88afe24/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/512x512bb.jpg',
+        descriptions: 'Gồm file 2 iPA Map Và Mod Skin Cập Nhật Ngày 16/8/2025'
+    },
+];
+
+const ksignApps = [
+    {
+        name: 'KSign ✅',
+        url: 'https://loadly.io/B4yjz3p6',
+        img: 'img/pf/ksign.png',
+        descriptions: 'GLOBAL TAKEOFF, INC'   
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/07DJmaef',
+        img: 'img/pf/ksign.png',
+        descriptions: 'Tianjin University of Commerce'   
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/57jdwiXt',
+        img: 'img/pf/ksign.png',
+        descriptions: 'BMW Brilliance Automotive Ltd'   
+    },
+    {
+        name: 'KSign BMW V1',
+        url: 'https://loadly.io/dKaQsrKZ',
+        img: 'img/pf/ksign.png',
+        descriptions: 'BMW Brilliance Automotive Ltd'   
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/vQvSH91u',
+        img: 'img/pf/ksign.png',
+        descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE'   
+    },
+    {
+        name: 'KSign VNJ V1',
+        url: 'https://loadly.io/YW8HSTgM',
+        img: 'img/pf/ksign.png',
+        descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE'   
+    },
+    {
+        name: 'KSign VNJ V2',
+        url: 'https://loadly.io/LiQXptmW',
+        img: 'img/pf/ksign.png',
+        descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE'   
+    },
+    {
+        name: 'KSign VNJ V3',
+        url: 'https://loadly.io/TADbykHo',
+        img: 'img/pf/ksign.png',
+        descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE'   
+    },
+    {
+        name: 'KSign VNJ V4',
+        url: 'https://loadly.io/UZO7qsci',
+        img: 'img/pf/ksign.png',
+        descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE'   
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/pJE8Gd6X',
+        img: 'img/pf/ksign.png',
+        descriptions: 'VIETTEL GROUP'   
+    },
+    {
+        name: 'KSign VIETTEL GROUP V1',
+        url: 'https://loadly.io/YqfVTduO',
+        img: 'img/pf/ksign.png',
+        descriptions: 'VIETTEL GROUP'   
+    },
+    {
+        name: 'KSign VIETTEL GROUP V2',
+        url: 'https://loadly.io/XYvyFUF5',
+        img: 'img/pf/ksign.png',
+        descriptions: 'VIETTEL GROUP'   
+    },
+    {
+        name: 'KSign VIETTEL GROUP V3',
+        url: 'https://loadly.io/yOvQTfs8',
+        img: 'img/pf/ksign.png',
+        descriptions: 'VIETTEL GROUP'   
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/6kFySVCG',
+        img: 'img/pf/ksign.png',
+        descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY'   
+    },
+    {
+        name: 'KSign Chiba V1',
+        url: 'https://loadly.io/zUyCT3H8',
+        img: 'img/pf/ksign.png',
+        descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY'   
+    },
+    {
+        name: 'KSign Chiaba V2',
+        url: 'https://loadly.io/kSkjPMhv',
+        img: 'img/pf/ksign.png',
+        descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY'   
+    },
+    {
+        name: 'KSign Chiaba V3',
+        url: 'https://loadly.io/NRkK7p17',
+        img: 'img/pf/ksign.png',
+        descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY'   
+    },
+    {
+        name: 'KSign Chiba V4',
+        url: 'https://loadly.io/gb2XXQOu',
+        img: 'img/pf/ksign.png',
+        descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY'   
+    },
+    {
+        name: 'KSign Chiba V5',
+        url: 'https://loadly.io/Mp7femdt',
+        img: 'img/pf/ksign.png',
+        descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY'   
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/ubn8wwZY',
+        img: 'img/pf/ksign.png',
+        descriptions: 'TCL household Appliance Marketing Co'   
+    },
+    {
+        name: 'KSign TCL V1',
+        url: 'https://loadly.io/nWsaaiiL',
+        img: 'img/pf/ksign.png',
+        descriptions: 'TCL household Appliance Marketing Co'   
+    },
+    {
+        name: 'KSign TCL V2',
+        url: 'https://loadly.io/6i1ZZSji',
+        img: 'img/pf/ksign.png',
+        descriptions: 'TCL household Appliance Marketing Co'   
+    },
+    {
+        name: 'KSign TCL V3',
+        url: 'https://loadly.io/nhMdwJJX',
+        img: 'img/pf/ksign.png',
+        descriptions: 'TCL household Appliance Marketing Co'
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/dbEiV4Fr',
+        img: 'img/pf/ksign.png',
+        descriptions: 'GAC TOYOTA MOTOR CO'
+    },
+    {
+        name: 'KSign GAC V1',
+        url: 'https://loadly.io/9TnVohcF',
+        img: 'img/pf/ksign.png',
+        descriptions: 'GAC TOYOTA MOTOR CO'
+    },
+    {
+        name: 'KSign GAC V2',
+        url: 'https://loadly.io/ZHFbgrf3',
+        img: 'img/pf/ksign.png',
+        descriptions: 'GAC TOYOTA MOTOR CO'
+    },
+    {
+        name: 'KSign GAC V3',
+        url: 'https://loadly.io/qP7wRRqS',
+        img: 'img/pf/ksign.png',
+        descriptions: 'GAC TOYOTA MOTOR CO'
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/WoTRbYH7',
+        img: 'img/pf/ksign.png',
+        descriptions: 'EEO Education Ltd'
+    },
+    {
+        name: 'KSign EEO V1',
+        url: 'https://loadly.io/vu7OjCg1',
+        img: 'img/pf/ksign.png',
+        descriptions: 'EEO Education Ltd'
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/SpCyGaYM',
+        img: 'img/pf/ksign.png',
+        descriptions: 'China Railway Eryuan Engineering Group'
+    },
+    {
+        name: 'KSign China Railway V1',
+        url: 'https://loadly.io/EArBL5WZ',
+        img: 'img/pf/ksign.png',
+        descriptions: 'China Railway Eryuan Engineering Group'
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/3psivxUU',
+        img: 'img/pf/ksign.png',
+        descriptions: 'Beijing Esensoft Co'
+    }
+];
+
+const esignApps = [
+   {
+        name: 'ESign ✅',
+        url: 'https://loadly.io/PGwC04B6',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'GLOBAL TAKEOFF, INC'   
+    },
+    {
+        name: 'ESign ',
+        url: 'https://loadly.io/g0FwZsy3',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'Tianjin University of Commerce'   
+    },
+    {
+        name: 'ESign ',
+        url: 'https://loadly.io/KDxT6fCO',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'BMW Brilliance Automotive Ltd'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/vnjes8ios',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/sCxKfNji',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'VIETTEL GROUP'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/NRqDTAah',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/8FnyqEjs',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'Beijing Esensoft Co., Ltd'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/ZQgRwTtA',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'NREH ESTATE INFORMATION TECHNOLOGY CO.,LTD'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/TM5Ye1O1',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'China Railway Eryuan Engineering Group Co., Ltd.'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/Rod6cWdW',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'EEO Education ltd'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/pokXH5G4',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'TCL household Appliance Marketing Co., LTD'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/zBXxhRoc',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'Wuling Power Corporation'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/55ZWv8OZ',
+        img: 'img/pf/profile-pic.png',
+        descriptions: 'GAC TOYOTA MOTOR CO.,LTD'
+    }
+    
+];
+
+// UI COMPONENTS
+
+const cloudIcon = `<svg class="icon" style="width: 24px;height: 24px;vertical-align: middle;fill: currentColor;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="739"><path d="M691.3 693.9c-24.7 0-44.8-20-44.8-44.8 0-24.7 20-44.8 44.8-44.8h40c190.6 0 176.8-274.4 0.2-268.1C671.2 37.2 208 145.1 299 445c-93-73.9-204.6 69.2-109.6 139.8 67.2 49.9 188.5-18.4 188.5 64.3 0 56.3-63.9 44.8-125.3 44.8-149.3 0-241.3-159.3-155.8-281.1 40.8-58.2 104.9-72.6 104.9-72.6C231.4 111.3 492.7-9.5 686.2 120c83.1 55.6 114.5 136.9 114.5 136.9 48.8 14.8 90.7 45.6 119.2 86.4 96 137.3 21.5 350.6-228.6 350.6zM514.2 335.8c0.2 0 0.3 0.1 0.5 0.1s0.3-0.1 0.5-0.1h-1z m177.1 421.1c0-17.5-45.8-17.3-63.3 0.1l-32.8 32.9c-14.1 14.1-38.2 4.2-38.2-15.8V380.6c0-24.5-17.9-44.4-42.3-44.7-24.5 0.3-47.2 20.1-47.2 44.7v393.5c0 19.9-24.1 29.9-38.2 15.8l-32.8-42.1c-17.5-17.5-45.3-9.3-62.8-9.3h-0.2c-17.5 44.8-17.6 55.1-0.1 72.6L449 931.5c35 35 91.6 37.3 126.6 2.3l115.7-114.6c17.5-17.5 0-45.2 0-62.7v0.4z" p-id="740"></path></svg>`;
+
+// DOM ELEMENTS
+
+const appContainer = document.querySelector('.app-section');
+const ksignContainer = document.querySelector('.ksign-section');
+const esignContainer = document.querySelector('.esign-section');
+
+// UTILITY FUNCTIONS
+
+function render(name, url, img, description) {
+    return `<div class="app-container">
+        <div class="app">
+            <img src="${img}" class="app-img box-shadow" alt="${name}">
+            <h1 style="margin-left: 1em">${name}<br>
+                <p>${description}</p>
+            </h1>
+        </div>
+        <i href="${url}" class="download-btn" style="color: #007aff;">
+            ${cloudIcon}
+        </i>
+    </div>`;
+}
+
+function addClickHandler(container) {
+    if (!container) return;
+    
+    container.addEventListener('click', (event) => {
+        const target = event.target.closest('.app-container');
+        if (target) {
+            const url = target.querySelector('.download-btn').getAttribute('href');
+            window.open(url, '_blank');
+        }
+    });
+}
+
+// INITIALIZATION
+
+// Render apps in their respective containers
+if (appContainer) {
+    apps.forEach(app => {
+        appContainer.innerHTML += render(app.name, app.url, app.img, app.descriptions);
+    });
+}
+
+if (ksignContainer) {
+    ksignApps.forEach(ksign => {
+        ksignContainer.innerHTML += render(ksign.name, ksign.url, ksign.img, ksign.descriptions);
+    });
+}
+
+if (esignContainer) {
+    esignApps.forEach(esign => {
+        esignContainer.innerHTML += render(esign.name, esign.url, esign.img, esign.descriptions);
+    });
+}
+
+// Add click handlers to all containers
+addClickHandler(appContainer);
+addClickHandler(ksignContainer);
+addClickHandler(esignContainer);
--- main.js
+++ main.js
@@ -1,81 +1,201 @@
 
-// Typing Effect
-const texts = ["Programmer. ", "Developer. ", "Seller. "];
-let count = 0, index = 0, isDeleting = false;
-const typedText = document.getElementById("typed-text");
-const typingSpeed = 100, deletingSpeed = 50, delayBetween = 2000;
+// CONFIGURATION & DATA
 
-function type() {
-  const current = texts[count];
-  typedText.textContent = isDeleting ? current.substring(0, index--) : current.substring(0, index++);
-  let speed = isDeleting ? deletingSpeed : typingSpeed;
+const TYPING_CONFIG = {
+    texts: ["Seller.", "Designer.", "Developer."],
+    typingSpeed: 100,
+    deletingSpeed: 50,
+    pauseBeforeDeleting: 2000
+};
 
-  if (!isDeleting && index === current.length) {
-    speed = delayBetween; isDeleting = true;
-  } else if (isDeleting && index === 0) {
-    isDeleting = false;
-    count = (count + 1) % texts.length;
-    speed = typingSpeed;
-  }
-  setTimeout(type, speed);
-}
-type();
-
-// Skill Bar Animation
-const skillFills = document.querySelectorAll(".skill-fill");
-function showSkills() {
-  const triggerBottom = window.innerHeight * 0.9;
-  skillFills.forEach(bar => {
-    const barTop = bar.getBoundingClientRect().top;
-    if (barTop < triggerBottom) bar.style.width = bar.getAttribute("data-percent");
-  });
-}
-window.addEventListener("scroll", showSkills);
-
-// Social Buttons
 const SOCIALS = [
-  {name:'facebook', url:'https://www.facebook.com/share/1AzKp6p2NJ/'},
-  {name:'tiktok', url:'https://tiktok.com/@plongdev_'},
-  {name:'messenger', url:'https://m.me/j/Abb7x2Ql4c0-_b_r/'},
-  {name:'zalo', url:'https://zalo.me/0372868397'}
+    {
+        name: 'facebook',
+        url: 'https://www.facebook.com/share/1XH7kThUjd/?mibextid=wwXIfr'
+    },
+    {
+        name: 'messenger',
+        url: 'https://m.me/khoindvn.esigncert'
+    },
+    {
+        name: 'telegram',
+        url: 'https://t.me/m/7P3IVuTSYTQ1'
+    },
+    {
+        name: 'zalo',
+        url: 'https://zalo.me/0386495528'
+    }
 ];
-const socialContainer = document.querySelector(".social-buttons");
-SOCIALS.forEach(s=>{
-  const btn = document.createElement("button");
-  btn.className="social-btn box-shadow";
-  btn.innerHTML=`<img src="assets/image/${s.name}.png" class="logo" alt="${s.name}"/>`;
-  btn.onclick = ()=>window.open(s.url,'_blank');
-  socialContainer.appendChild(btn);
-});
 
-// Dark Mode Toggle
-const colorModeBtn = document.getElementById("colorMode");
-let darkMode = false;
-let rotation = 0; // góc hiện tại
+// TYPING ANIMATION STATE
 
-colorModeBtn.addEventListener("click", () => {
-  darkMode = !darkMode;
-  document.body.classList.toggle("dark-mode", darkMode);
+let textIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
 
-  // luân phiên chiều quay: lần lẻ → +180, lần chẵn → -180
-  rotation += (rotation % 360 === 0 ? 180 : -180);
+// UTILITY FUNCTIONS
 
-  colorModeBtn.style.transform = `rotate(${rotation}deg)`;
-});
-
-
-// Menu toggle for mobile
-const menuButton = document.getElementById("menu-button");
-const navLinks = document.querySelector(".nav-links");
-menuButton.addEventListener("click", ()=>navLinks.classList.toggle("show"));
-
-// Entrance animation
-const enterEl = document.querySelector(".enter");
-if(enterEl){
-  for(let i=20;i>=0;i--){
-    setTimeout(()=>{
-      enterEl.style.backdropFilter = `blur(${i}px)`;
-      enterEl.style.WebkitBackdropFilter = `blur(${i}px)`;
-    },40*(20-i));
-  }
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
 }
+
+// TYPING ANIMATION
+
+function typeText() {
+    const currentText = TYPING_CONFIG.texts[textIndex];
+    const typedTextElement = document.getElementById("typed-text");
+    
+    if (!isDeleting && charIndex < currentText.length) {
+        typedTextElement.innerHTML = currentText.substring(0, charIndex + 1);
+        charIndex++;
+        setTimeout(typeText, TYPING_CONFIG.typingSpeed);
+    } else if (isDeleting && charIndex > 0) {
+        typedTextElement.innerHTML = currentText.substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(typeText, TYPING_CONFIG.deletingSpeed);
+    } else if (!isDeleting && charIndex === currentText.length) {
+        isDeleting = true;
+        setTimeout(typeText, TYPING_CONFIG.pauseBeforeDeleting);
+    } else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        textIndex = (textIndex + 1) % TYPING_CONFIG.texts.length;
+        setTimeout(typeText, TYPING_CONFIG.typingSpeed);
+    }
+}
+
+// SKILL BARS ANIMATION
+
+function animateSkillBars() {
+    const skillBars = document.querySelectorAll('.skill-fill');
+    
+    skillBars.forEach(bar => {
+        const percent = bar.getAttribute('data-percent');
+        bar.style.width = percent;
+    });
+}
+
+function handleSkillsAnimation() {
+    const skillSection = document.querySelector('.skills-section');
+    if (isInViewport(skillSection)) {
+        animateSkillBars();
+        window.removeEventListener('scroll', handleSkillsAnimation);
+    }
+}
+
+// NAVIGATION MENU
+
+function initializeMenuToggle() {
+    const menuButton = document.getElementById('menu-button');
+    const navLinks = document.querySelector('.nav-links');
+    
+    if (menuButton && navLinks) {
+        menuButton.addEventListener('click', function() {
+            navLinks.classList.toggle('show');
+            navLinks.classList.toggle('box-shadow');
+        });
+    }
+}
+
+// SOCIAL BUTTONS
+
+function createSocialButtons() {
+    const socialButtonContainer = document.querySelector('.social-buttons');
+    
+    if (!socialButtonContainer) return;
+    
+    SOCIALS.forEach(social => {
+        const button = document.createElement('button');
+        button.classList.add('social-btn', 'box-shadow');
+        button.innerHTML = `<img src="img/logo/${social.name}.png" class="logo" alt="${social.name}"/>`;
+        button.addEventListener('click', () => {
+            window.open(social.url, '_blank');
+        });
+        socialButtonContainer.appendChild(button);
+    });
+}
+
+// DARK MODE TOGGLE
+
+function initializeDarkModeToggle() {
+    const colorModeButton = document.querySelector('#colorMode, .color-mode, [data-color-mode]');
+    
+    if (!colorModeButton) return;
+    
+    colorModeButton.addEventListener('click', toggleDarkMode);
+}
+
+function toggleDarkMode() {
+    const elements = {
+        body: document.body,
+        skillsSection: document.querySelector('.skills-section'),
+        wave1: document.querySelector('#wave1'),
+        wave2: document.querySelector('#wave2'),
+        svg1: document.querySelector('.svg1'),
+        svg2: document.querySelector('.svg2')
+    };
+    
+    const currentDarkMode = document.body.style.getPropertyValue("--dark-mode") - 0;
+    const isDarkMode = Boolean(currentDarkMode);
+    
+    elements.body.style.setProperty("--dark-mode", 1 - currentDarkMode);
+    
+    elements.body.style.color = isDarkMode ? 'black' : 'white';
+    elements.body.style.background = isDarkMode 
+        ? 'linear-gradient(0deg, #94bbff, #fff)' 
+        : 'linear-gradient(0deg, #777272, #000)';
+    
+    if (elements.skillsSection) {
+        elements.skillsSection.style.backgroundColor = isDarkMode ? 'white' : 'black';
+    }
+    
+    if (elements.svg1) {
+        elements.svg1.style.fill = isDarkMode ? 'white' : 'black';
+    }
+    
+    if (elements.wave2) {
+        elements.wave2.style.backgroundColor = isDarkMode ? 'white' : 'black';
+    }
+    
+    if (elements.svg2) {
+        elements.svg2.style.fill = isDarkMode ? '#f4f4f4' : 'black';
+    }
+}
+
+// ENTRANCE ANIMATION
+
+function playEntranceAnimation() {
+    const enterElement = document.querySelector('.enter');
+    if (!enterElement) return;
+    
+    for (let i = 20; i >= 0; i--) {
+        setTimeout(() => {
+            const blurValue = `blur(${i}px)`;
+            enterElement.style.backdropFilter = blurValue;
+            enterElement.style.WebkitBackdropFilter = blurValue;
+        }, 40 * (20 - i));
+    }
+}
+
+// INITIALIZATION
+
+function initializeApp() {
+    initializeMenuToggle();
+    createSocialButtons();
+    initializeDarkModeToggle();
+    playEntranceAnimation();
+    
+    typeText();
+    handleSkillsAnimation();
+    
+    window.addEventListener('scroll', handleSkillsAnimation);
+}
+
+// EVENT LISTENERS
+
+document.addEventListener('DOMContentLoaded', initializeApp);
